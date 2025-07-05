933c5821334ad738cfd5903cbcdda000a686e61d
KhakimzhonovUB <khakimzhonovub@gmail.com>
2025-07-05 15:13:55 +0500

    add exceptions to Domain.Exceptions

---
+++ src/TodoApp.Domain/Exceptions/DomainException.cs
@@ -0,0 +1,35 @@
+﻿namespace TodoApp.Domain.Exceptions;
+
+/// <summary>
+/// Базовое доменное исключение
+/// </summary>
+public class DomainException : Exception
+{
+    /// <summary>
+    /// Код ошибки
+    /// </summary>
+    public string ErrorCode { get; }
+
+    /// <summary>
+    /// Создать новое доменное исключение
+    /// </summary>
+    /// <param name="message">Сообщение об ошибке</param>
+    /// <param name="errorCode">Код ошибки</param>
+    public DomainException(string message, string errorCode = "DOMAIN_ERROR")
+        : base(message)
+    {
+        ErrorCode = errorCode;
+    }
+
+    /// <summary>
+    /// Создать новое доменное исключение с внутренним исключением
+    /// </summary>
+    /// <param name="message">Сообщение об ошибке</param>
+    /// <param name="innerException">Внутреннее исключение</param>
+    /// <param name="errorCode">Код ошибки</param>
+    public DomainException(string message, Exception innerException, string errorCode = "DOMAIN_ERROR") 
+        : base(message, innerException)
+    {
+        ErrorCode = errorCode;
+    }
+}
---
+++ src/TodoApp.Domain/Exceptions/NotFoundException.cs
@@ -0,0 +1,42 @@
+﻿namespace TodoApp.Domain.Exceptions;
+
+/// <summary>
+/// Исключение, возникающее, когда сущность не найдена
+/// </summary>
+public class NotFoundException : DomainException
+{
+    /// <summary>
+    /// Тип сущности
+    /// </summary>
+    public string EntityType { get; }
+    
+    /// <summary>
+    /// Идентификатор сущности
+    /// </summary>
+    public Guid EntityId { get; }
+
+    /// <summary>
+    /// Создать исключение для ненайденной сущности
+    /// </summary>
+    /// <param name="entityType">Тип сущности</param>
+    /// <param name="entityId">Идентификатор сущности</param>
+    public NotFoundException(string entityType, Guid entityId)
+        : base($"{entityType} с идентификатором '{entityId}' не найдена", "ENTITY_NOT_FOUND")
+    {
+        EntityType = entityType;
+        EntityId = entityId;
+    }
+
+    /// <summary>
+    /// Создать исключение для ненайденной сущности с внутренним исключением
+    /// </summary>
+    /// <param name="entityType">Тип сущности</param>
+    /// <param name="entityId">Идентификатор сущности</param>
+    /// <param name="innerException">Внутреннее исключение</param>
+    public NotFoundException(string entityType, Guid entityId, Exception innerException)
+        : base($"{entityType} с идентификатором '{entityId}' не найдена", innerException, "ENTITY_NOT_FOUND")
+    {
+        EntityType = entityType;
+        EntityId = entityId;
+    }
+}
