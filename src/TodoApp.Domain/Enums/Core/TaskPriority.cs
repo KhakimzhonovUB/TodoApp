4473b42d9802765f92dd99bb050e24ee018b6009
KhakimzhonovUB <khakimzhonovub@gmail.com>
2025-07-05 19:54:24 +0500

    add TaskPriority and his extension to Domain.Enums

---
+++ src/TodoApp.Domain/Enums/Core/TaskPriority.cs
@@ -0,0 +1,32 @@
+﻿namespace TodoApp.Domain.Enums;
+
+/// <summary>
+/// Перечисление приоритетов задач
+/// </summary>
+public enum TaskPriority
+{
+    /// <summary>
+    /// Очень низкий приоритет
+    /// </summary>
+    VeryLow = 1,
+    
+    /// <summary>
+    /// Низкий приоритет
+    /// </summary>
+    Low = 2,
+    
+    /// <summary>
+    /// Средний приоритет (по умолчанию)
+    /// </summary>
+    Medium = 3,
+    
+    /// <summary>
+    /// Высокий приоритет
+    /// </summary>
+    High = 4,
+    
+    /// <summary>
+    /// Критический приоритет
+    /// </summary>
+    Critical = 5
+}
---
+++ src/TodoApp.Domain/Enums/Extensions/TaskPriorityExtensions.cs
@@ -0,0 +1,48 @@
+﻿namespace TodoApp.Domain.Enums;
+
+/// <summary>
+/// Методы расширения для работы с TaskPriority
+/// </summary>
+public static class TaskPriorityExtensions
+{
+    /// <summary>
+    /// Получить отображаемое имя приоритета
+    /// </summary>
+    /// <param name="priority">Приоритет задачи</param>
+    /// <returns>Отображаемое имя приоритета</returns>
+    public static string GetDisplayName(this TaskPriority priority) =>
+        priority switch
+        {
+            TaskPriority.VeryLow => "Very Low",
+            TaskPriority.Low => "Low",
+            TaskPriority.Medium => "Medium",
+            TaskPriority.High => "High",
+            TaskPriority.Critical => "Critical",
+            _ => throw new ArgumentOutOfRangeException(
+                nameof(priority), priority, "Неизвестный приоритет задачи")
+        };
+    
+    /// <summary>
+    /// Получить описание приоритета
+    /// </summary>
+    /// <param name="priority">Приоритет задачи</param>
+    /// <returns>Описание приоритета</returns>
+    public static string GetDescription(this TaskPriority priority) =>
+        priority switch
+        {
+            TaskPriority.VeryLow => "Задача с очень низким приоритетом, может быть выполнена в последнюю очередь",
+            TaskPriority.Low => "Задача с низким приоритетом, не требует срочного выполнения",
+            TaskPriority.Medium => "Задача со средним приоритетом, стандартное время выполнения",
+            TaskPriority.High => "Задача с высоким приоритетом, требует быстрого выполнения",
+            TaskPriority.Critical => "Критическая задача, требует немедленного внимания",
+            _ => throw new ArgumentOutOfRangeException(
+                nameof(priority), priority, "Неизвестный приоритет задачи")
+        };
+    
+    /// <summary>
+    /// Получить все доступные приоритеты в порядке убывания важности
+    /// </summary>
+    /// <returns>Коллекция приоритетов</returns>
+    public static IEnumerable<TaskPriority> GetPriorities() =>
+        [TaskPriority.Critical, TaskPriority.High, TaskPriority.Medium, TaskPriority.Low, TaskPriority.VeryLow];
+}
